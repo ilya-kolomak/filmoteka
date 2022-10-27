0067bb405c5709dd99518cf5fa49f89623485357
ilya-kolomak <99127402+ilya-kolomak@users.noreply.github.com>
2022-10-27 22:25:41 +0200

    Update recuest-popular-movies.js

--- src/js/recuest-popular-movies.js
+++ src/js/recuest-popular-movies.js
@@ -1,25 +1,16 @@
-import axios from 'axios';
-axios.defaults.baseURL = 'https://api.themoviedb.org/';
-const API_KEY = '05e64fd21cd8a0d5400571b79e99a2f3';
-export class MoviesAPi {
-  #page = 1;
-  #totalPages = 0;
+const url =
+  ' https://api.themoviedb.org/3/trending/movie/week?api_key=05e64fd21cd8a0d5400571b79e99a2f3';
 
-  async getPhotos() {
-    const url = `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`;
-
-    const data = await axios.get(url);
-    return data;
-  }
-
-  incrementPage() {
-    this.#page += 1;
-  }
-  dicrementPage() {
-    this.#page -= 1;
-  }
-
-  resetPage() {
-    this.#page = 1;
-  }
-}
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Data handling
+  })
+  .catch(error => {
+    // Error handling
+  });
