7edf37dc38f04b567555a9a7887eda320ae7cc4e
Nikita Polozov <npolozov0@gmail.com>
2022-10-27 11:53:51 +0300

    зробив пагинацию

---
+++ src/js/pagination.js
@@ -0,0 +1,39 @@
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+
+const paginationContainer = document.querySelector('.tui-pagination');
+console.log(paginationContainer);
+
+const options = {
+  totalItems: 100,
+  itemsPerPage: 10,
+  visiblePages: 5,
+  page: 1,
+  centerAlign: true,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{page}}">' +
+      '<span class="tui-ico-{{type}}">{{page}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
+};
+
+const createPagination = () => {
+  const pagination = new Pagination(paginationContainer, options);
+  return pagination;
+};
+
+export default createPagination;
