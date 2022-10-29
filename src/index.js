385b7b54941c0333873d8ebfa8dc8f1b8ceb575d
Michael <mitru0217@gmail.com>
2022-10-29 06:39:21 +0300

    in process
    Не получается сделать map для merged

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import ImageApiService from './js/mdApiService';
-import card from './templates/card.hbs';
+import GenresApiService from './js/genresApi';
 
+const STORAGE_KEY_GENRES = `genres`;
+const STORAGE_KEY_RESULTS = `results`;
 let photosContainer = document.querySelector('.js-photos-container');
-
+let merged = {};
 const form = document.querySelector('.js-search-form');
 const searchBtn = document.querySelector('.js-search-btn');
 
 const imageApiService = new ImageApiService();
-
+const genresApiService = new GenresApiService();
 form.addEventListener('submit', onSearch);
 
 function onSearch(e) {
@@ -15,16 +17,55 @@ function onSearch(e) {
 
   imageApiService.query = e.currentTarget.elements.query;
 
-  imageApiService.resetPage();
-
   imageApiService.fetchImages().then(({ results }) => {
-    appendPhotosMarkup(results);
-
-    console.log(results);
+    localStorage.setItem(STORAGE_KEY_RESULTS, JSON.stringify({ results }));
+    return results;
   });
+
+  genresApiService.fetchGenres().then(({ genres }) => {
+    localStorage.setItem(STORAGE_KEY_GENRES, JSON.stringify({ genres }));
+    return genres;
+  });
+
+  renderMarkupCard();
 }
 
-function appendPhotosMarkup(results) {
-  photosContainer.insertAdjacentHTML('beforeend', card(results));
-  console.log(photosContainer);
+function addGenresToResults() {
+  const savedResults = JSON.parse(localStorage.getItem(STORAGE_KEY_RESULTS));
+  const savedGenres = JSON.parse(localStorage.getItem(STORAGE_KEY_GENRES));
+  console.log(savedGenres);
+  console.log(savedResults);
+
+  let merged = { ...savedResults, ...savedGenres };
+
+  console.log(merged);
+  return merged;
+}
+
+function renderMarkupCard(obj) {
+  addGenresToResults();
+
+  const markup = obj.merged
+    .map(
+      ({
+        results: { poster_path, title, original_title, release_date },
+        genres: { name },
+      }) => {
+        return ` <li class="hero-item">
+    <div class="hero-thumb">
+    <img
+     src="${poster_path}"
+    alt="${title}" loading="lazy" class="img-item" />
+    </div>
+    <div class="hero-info">
+      <p class="film-title">${original_title}</p>
+      <p class="film-info">${name}|</p>
+      <p class="film-info">${release_date}</p>
+    </div>
+   </li>`;
+      }
+    )
+    .join('');
+
+  photosContainer.insertAdjacentHTML('beforeend', markup);
 }
--- src/js/mdApiService.js
+++ src/js/mdApiService.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 // const axios = require('axios').default;
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // import 'notiflix/dist/notiflix-3.2.5.min.css';
-const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
-const API_KEY = '05e64fd21cd8a0d5400571b79e99a2f3';
+// const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+// const API_KEY = '05e64fd21cd8a0d5400571b79e99a2f3';
 
 export default class ImageApiService {
   constructor() {
@@ -19,15 +19,6 @@ export default class ImageApiService {
       this.page += 1;
       // console.log(response.data);
       return response.data;
-    } catch (error) {}
-  }
-
-  async fetchGenres() {
-    try {
-      const response = await axios.get(
-        'https://api.themoviedb.org/3/genre/movie/list?api_key=38f6f2c88436f6a6fb5d137cfc7b2688&language=en-US'
-      );
-      console.log(response.data);
     } catch (error) {}
   }
   resetPage() {
