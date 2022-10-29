c808cc789f749e3ab81475c5fea794eee93d7c23
Nikita Polozov <npolozov0@gmail.com>
2022-10-29 16:35:17 +0300

    1

--- src/js/recuest-popular-movies.js
+++ src/js/recuest-popular-movies.js
@@ -1,21 +1,4 @@
-// const url =
-//   ' https://api.themoviedb.org/3/trending/movie/week?api_key=05e64fd21cd8a0d5400571b79e99a2f3';
 
-// fetch(url)
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   })
-//   .then(data => {
-//     console.log(data);
-
-//     // Data handling
-//   })
-//   .catch(error => {
-//     // Error handling
-//   });
 
 import axios from 'axios';
 
@@ -29,26 +12,3 @@ export class MoviesApi {
     return data;
   }
 }
-<<<<<<< HEAD
-=======
-// class MoviesApi {
-//   getpopularFilms() {
-//     return fetch(url)
-//       .then(response => {
-//         if (!response.ok) {
-//           throw new Error(response.status);
-//         }
-//         return response.json();
-//       })
-//       .then(data => {
-//         console.log(data);
-
-//         // Data handling
-//       })
-//       .catch(error => {
-//         // Error handling
-//       });
-
-//   }
-// }
->>>>>>> 884446346d1d359c150d0f6d3e0cca7e2e695e77
