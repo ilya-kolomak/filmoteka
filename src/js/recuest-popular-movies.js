8268bc715a40d60e53d31d022b472c9a95bd6e72
Nikita Polozov <npolozov0@gmail.com>
2022-10-29 12:01:17 +0300

    Update recuest-popular-movies.js

--- src/js/recuest-popular-movies.js
+++ src/js/recuest-popular-movies.js
@@ -1,6 +1,7 @@
 // const url =
 //   ' https://api.themoviedb.org/3/trending/movie/week?api_key=05e64fd21cd8a0d5400571b79e99a2f3';
 
+<<<<<<< Updated upstream
 // fetch(url)
 //   .then(response => {
 //     if (!response.ok) {
@@ -27,5 +28,24 @@ export class MoviesApi {
     const { data } = await axios.get(url);
 
     return data;
+=======
+class MoviesApi {
+  getpopularFilms() {
+    return fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+
+        // Data handling
+      })
+      .catch(error => {
+        // Error handling
+      });
+>>>>>>> Stashed changes
   }
 }
