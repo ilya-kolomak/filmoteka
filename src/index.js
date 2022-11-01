cdfcbe1630f289b1a2007b48e07b8809f86c48c6
Nikita Polozov <npolozov0@gmail.com>
2022-11-01 18:52:29 +0200

    сделал кнопку переключения фона

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './js/slider';
 import './js/hero';
 import './js/renderPagination';
 import './js/modal-dreamTeam';
+import './js/change-theme';
 
 import './js/my-library-btn';
 import './js/modal-btn';
