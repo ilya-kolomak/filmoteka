a650c55f9e7ab4a60b4b1188ab1157e2db06aa8e
VerbitskaAlisa <v.alica@ukr.net>
2022-11-03 17:58:03 +0200

    margins

--- src/js/onFormSearch.js
+++ src/js/onFormSearch.js
@@ -2,7 +2,6 @@ import ImageApiService from './mdApiService';
 import { refs } from './refs';
 import renderMarkupCard from './hero';
 import { clearPagination } from './renderPagination';
-import createPagination from './pagination';
 
 let photosContainer = refs.photosContainer;
 const imageApiService = new ImageApiService();
@@ -11,18 +10,11 @@ refs.form && refs.form.addEventListener('submit', onSearch);
 async function onSearch(e) {
   e.preventDefault();
 
-  // const {
-  //   elements: { searchQuery },
-  // } = e.currentTarget;
-
   const query = e.target.searchQuery.value.trim().toLowerCase();
   if (!query) {
     return;
   }
-
   imageApiService.searchQuery = query;
-  // console.log('imageApiService :>> ', imageApiService);
-  console.log('imageApiService :>> ', imageApiService.page);
   clearPage();
   try {
     const { results, total_results } =
